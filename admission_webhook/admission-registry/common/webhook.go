88596721fc9d586d7b860d0b15e058fb5f240324
root <root@localhost.localdomain>
2023-01-13 04:40:00 +0800

    admission webhook开发完毕

---
+++ admission_webhook/admission-registry/common/webhook.go
@@ -0,0 +1,170 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	admissionV1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/klog"
+	"net/http"
+	"strings"
+)
+
+var (
+	runtimeSchema = runtime.NewScheme()
+	// admission数据使用的序列化
+	codeFactory = serializer.NewCodecFactory(runtimeSchema)
+	//反序列化,也就是解码
+	deserializer = codeFactory.UniversalDeserializer()
+)
+
+// webhook server的参数
+type WebhookSrvParam struct {
+	Port     int
+	CertFile string
+	KeyFile  string
+}
+
+// 使用原生net/http实现,也可以使用框架gin等
+// 封装一个webhook server
+type WebhookSrv struct {
+	Server          *http.Server
+	RegistryWhiteIp []string
+	// 请求的白名单
+}
+
+func (w *WebhookSrv) Handler(writer http.ResponseWriter, request *http.Request) {
+	var body []byte
+	if request.Body != nil {
+		if body, err := ioutil.ReadAll(request.Body); err != nil {
+			if len(body) == 0 {
+				klog.Error("空的body")
+				// http响应
+				http.Error(writer, "空的body", http.StatusBadRequest)
+				return
+			}
+			klog.Error("获取body信息失败")
+			http.Error(writer, "空的body", http.StatusBadRequest)
+			return
+		}
+	}
+	body = body
+	// 校验content-type
+	// 从准入控制器传递过来给我们自定义的admission webhook 是json字符串,但实际格式是admission review
+	if contentType := request.Header.Get("Content-Type"); contentType != "application/json" {
+		klog.Error("content-type is %s ,but expect application/json", contentType)
+		http.Error(writer, "content-type error ,expect application/json", http.StatusBadRequest)
+	}
+
+	// validate mutate请求的数据(交互,与admission controller之间,请求响应都是AdmissionReview)
+	// 数据编码
+	//准入控制器发送request也就是AdmissionRequstReview给我们admission webhook,反之就是admissionReponseReview
+	var admissionResponse *admissionV1.AdmissionResponse
+
+	// 获取请求过来的admissionReview
+	requestAdmissionReview := admissionV1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &requestAdmissionReview); err != nil {
+		klog.Error("未能正确解码body成admissionReview数据")
+		admissionResponse = &admissionV1.AdmissionResponse{
+			Result: metav1.Status{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			},
+		}
+	} else {
+		// 序列化成功,也就是成功拿到AdmissionRequestReview
+		if request.URL.Path == "/mutate" {
+
+		} else if request.URL.Path == "/validate" {
+			admissionResponse = w.validate(&requestAdmissionReview)
+		}
+	}
+
+	// 构造返回的admissionReview
+	responseAdmissionReview := admissionV1.AdmissionReview{}
+	// admission/v1
+	// 指定版本
+	responseAdmissionReview.APIVersion = requestAdmissionReview.APIVersion
+	responseAdmissionReview.Kind = requestAdmissionReview.Kind
+
+	// 有返回值
+	if admissionResponse != nil {
+		responseAdmissionReview.Response = admissionResponse
+		if requestAdmissionReview.Request != nil {
+			// 请求和返回的admission review的uid要一致,表明是一次请求
+			responseAdmissionReview.Response.UID = requestAdmissionReview.Request.UID
+		}
+	}
+	klog.Info(fmt.Sprintf("正在返回reponse数据: %v"), responseAdmissionReview.Response)
+	respBytes, err := json.Marshal(responseAdmissionReview)
+	if err != nil {
+		klog.Error("response admission review不能正确编码json")
+		http.Error(writer, fmt.Sprintf("response admission review不能正确编码json: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	klog.Info("响应准备好了...")
+	// 返回响应
+	if _, err := writer.Write(respBytes); err != nil {
+		klog.Error("返回响应失败: %v", err)
+		klog.Error(writer, fmt.Sprintf("返回响应失败: %v", err), http.StatusInternalServerError)
+	}
+}
+
+// 返回admissionResponse
+func (w *WebhookSrv) validate(ar *admissionV1.AdmissionReview) *admissionV1.AdmissionResponse {
+	req := ar.Request
+	var (
+		allowed = true
+		code    = http.StatusOK
+		msg     = ""
+	)
+	// req.Kind是admission review的group version kind
+	klog.Info("request admission review: kind=%s,ns=%s,name=%s,uid=%s", req.Kind.Kind, req.Namespace, req.Name, req.UID)
+
+	// 规则,判断镜像仓库,也就是校验pod
+	var pod corev1.Pod
+	// 获取原始的数据
+	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+		klog.Error("未能获取object的raw数据: %v", err)
+		allowed = false
+		code = http.StatusBadRequest
+		return &admissionV1.AdmissionResponse{
+			Allowed: allowed,
+			Result: &metav1.Status{
+				Code:    int32(code),
+				Message: err.Error(),
+			},
+		}
+	}
+
+	// 业务逻辑
+	for _, container := range pod.Spec.InitContainers {
+		var whiteList = false
+		for _, whiteIP := range w.RegistryWhiteIp {
+			// 前缀
+			if strings.HasPrefix(container.Image, whiteIP) {
+				// 命中白名单
+				whiteList = true
+			}
+			// 没有命中白名单
+			if !whiteList {
+				allowed = false
+				code = http.StatusFound
+				msg = fmt.Sprintf("%s image来自的镜像仓库不受信任,只信任来自%s的镜像", container.Image, w.RegistryWhiteIp)
+				break // 任意一个都可以处理了
+			}
+		}
+	}
+	return &admissionV1.AdmissionResponse{
+		Allowed: allowed,
+		Result: &metav1.Status{
+			Code:    int32(code),
+			Message: msg,
+		},
+	}
+}
---
+++ admission_webhook/admission-registry/main.go
@@ -0,0 +1,76 @@
+package admission_registry
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"k8s.io/klog"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+	"webhookDo/common"
+)
+
+func main() {
+	var param common.WebhookSrvParam
+	//生产环境很多都是加密
+	//加密端口 证书 私钥文件
+	flag.IntVar(&param.Port, "port", 443, "webhook server port")
+	flag.StringVar(&param.CertFile, "tlsCertFile", "/etc/wehook/certstls.crt", "x509 certfile")
+	flag.StringVar(&param.KeyFile, "tlsKeyFile", "/etc/webhook/tls.key", "x509 private key file")
+	flag.Parse()
+
+	//处理证书和私钥,类似与颁发证书
+	cert, err := tls.LoadX509KeyPair(param.CertFile, param.KeyFile)
+	if err != nil {
+		klog.Errorf("获取证书失败: %v", err)
+		return
+	}
+
+	WebhookServer := common.WebhookSrv{
+		Server: &http.Server{
+			Addr: fmt.Sprintf(":%d", param.Port),
+			TLSConfig: &tls.Config{
+				Certificates: []tls.Certificate{
+					cert,
+				},
+			},
+		},
+		RegistryWhiteIp: strings.Split(os.Getenv("WHITEIPS_REGISTRY"), ","),
+	}
+
+	// 创建http服务的handler
+	// http请求的多路复用器
+	mux := http.NewServeMux()
+	mux.HandleFunc("/validate", WebhookServer.Handler)
+	mux.HandleFunc("/mutate", WebhookServer.Handler)
+	WebhookServer.Server.Handler = mux
+
+	go func() {
+		// 本身这里就会阻塞
+		if err := WebhookServer.Server.ListenAndServeTLS("", "")err != nil {
+			klog.Errorf("启动server失败 %v", err)
+		}
+	}()
+
+	klog.Errorf("server启动了")
+
+	quit := make(chan os.Signal, 2)
+	//SIGINT SIGTERM
+	signal.Notify(quit, os, os.Interrupt)
+	<-quit
+	klog.Errorf("优雅关闭...")
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := WebhookServer.Server.Shutdown(ctx); err != nil {
+			klog.Errorf("server关闭失败")
+		}
+		klog.Errorf("server优雅关闭完成")
+	}()
+	<-quit
+	os.Exit(1)
+}
