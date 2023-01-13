762bd1efb5d80d04608841044cd8601ea2811f44
cjq <3472189068@qq.com>
2023-01-13 22:19:38 +0800

    mutate准入控制器实现完毕

--- admission_webhook/admission-registry/common/webhook.go
+++ admission_webhook/admission-registry/common/webhook.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	admissionV1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -12,6 +13,23 @@ import (
 	"k8s.io/klog"
 	"net/http"
 	"strings"
+)
+
+// 局部
+type patchOperation struct {
+	// 操作
+	OP string `json:"op"`
+	// 操作的路径
+	Path string `json:"path"`
+	// 值
+	Value interface{} `json:"value,omitempty"` // 可以不存在
+}
+
+const (
+	//annotation来标识是否需要mutate   =no/false/n/off
+	AnnotationMutateKey = "io.ydzs.admission-registry/mutate"
+	// 已经mutate了  =mutated
+	AnnotationStatusKey = "io.ydzs.admission-registry/status"
 )
 
 var (
@@ -82,7 +100,7 @@ func (w *WebhookSrv) Handler(writer http.ResponseWriter, request *http.Request) 
 	} else {
 		// 序列化成功,也就是成功拿到AdmissionRequestReview
 		if request.URL.Path == "/mutate" {
-
+			admissionResponse = w.mutate(&requestedAdmissionReview)
 		} else if request.URL.Path == "/validate" {
 			admissionResponse = w.validate(&requestAdmissionReview)
 		}
@@ -172,4 +190,135 @@ func (w *WebhookSrv) validate(ar *admissionV1.AdmissionReview) *admissionV1.Admi
 			Message: msg,
 		},
 	}
+}
+
+func (s *WebhookSrv) mutate(ar *admissionV1.AdmissionReview) *admissionV1.AdmissionResponse {
+	req := ar.Request
+	var (
+		// annotation信息
+		objectMeta                      *metav1.ObjectMeta
+		resourceNamespace, resourceName string
+	)
+
+	klog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v UID=%v Operation=%v",
+		req.Kind.Kind, req.Namespace, req.Name, req.UID, req.Operation)
+
+	switch req.Kind.Kind {
+	case "Deployment":
+		var deployment appsv1.Deployment
+		if err := json.Unmarshal(req.Object.Raw, &deployment); err != nil {
+			klog.Errorf("Could not unmarshal raw object: %v", err)
+			return &admissionV1.AdmissionResponse{
+				Result: &metav1.Status{
+					Code:    http.StatusBadRequest,
+					Message: err.Error(),
+				},
+			}
+		}
+		resourceName, resourceNamespace, objectMeta = deployment.Name, deployment.Namespace, &deployment.ObjectMeta
+	case "Service":
+		var service corev1.Service
+		if err := json.Unmarshal(req.Object.Raw, &service); err != nil {
+			klog.Errorf("Could not unmarshal raw object: %v", err)
+			return &admissionV1.AdmissionResponse{
+				Result: &metav1.Status{
+					Code:    http.StatusBadRequest,
+					Message: err.Error(),
+				},
+			}
+		}
+		resourceName, resourceNamespace, objectMeta = service.Name, service.Namespace, &service.ObjectMeta
+	default:
+		return &admissionV1.AdmissionResponse{
+			Result: &metav1.Status{
+				Code:    http.StatusBadRequest,
+				Message: fmt.Sprintf("Can't handle this kind(%s) object", req.Kind.Kind),
+			},
+		}
+	}
+
+	// 是否需要mutate或者是否已经mutate
+	if !mutationRequired(objectMeta) {
+		klog.Infof("Skipping validation for %s/%s due to policy check", resourceNamespace, resourceName)
+		return &admissionV1.AdmissionResponse{
+			// 直接通过即可
+			Allowed: true,
+		}
+	}
+
+	annotations := map[string]string{AnnotationStatusKey: "mutated"}
+
+	// 可以是多个操作
+	var patch []patchOperation
+	// ...多个
+	patch = append(patch, updateAnnotation(objectMeta.GetAnnotations(), annotations)...)
+
+	patchBytes, err := json.Marshal(patch)
+	if err != nil {
+		return &admissionV1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	}
+
+	klog.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
+	return &admissionV1.AdmissionResponse{
+		Allowed: true,
+		Patch:   patchBytes,
+		// admissionV1.PatchTypeJSONPatch指针,值JSONPatch字符串
+		PatchType: func() *admissionV1.PatchType {
+			pt := admissionV1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
+}
+
+func mutationRequired(metadata *metav1.ObjectMeta) bool {
+	annotations := metadata.GetAnnotations()
+	// 没有创建annotation
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+
+	var required bool
+	switch strings.ToLower(annotations[AnnotationMutateKey]) {
+	default:
+		required = true
+	case "n", "no", "false", "off":
+		required = false
+	}
+	status := annotations[AnnotationStatusKey]
+
+	if strings.ToLower(status) == "mutated" {
+		required = false
+	}
+
+	klog.Infof("Mutation policy for %v/%v: required:%v", metadata.Namespace, metadata.Name, required)
+	return required
+}
+
+func updateAnnotation(target map[string]string, added map[string]string) (patch []patchOperation) {
+	for key, value := range added {
+		// 没有annotation
+		if target == nil || target[key] == "" {
+			target = map[string]string{}
+			patch = append(patch, patchOperation{
+				OP:   "add",                   // 第一次操作新增
+				Path: "/metadata/annotations", // 操作的路径
+				// key value
+				Value: map[string]string{
+					key: value,
+				},
+			})
+		} else {
+			patch = append(patch, patchOperation{
+				OP: "replace",
+				// 添加容器那就是/spec/containers/
+				Path:  "/metadata/annotations/" + key,
+				Value: value,
+			})
+		}
+	}
+	return patch
 }
