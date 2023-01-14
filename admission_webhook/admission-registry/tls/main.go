9a05d11d5bf47ce592fa6d0b3c26b0ae4982afe4
cjq <3472189068@qq.com>
2023-01-14 11:55:50 +0800

    自动注册admission webhook

--- admission_webhook/admission-registry/tls/main.go
+++ admission_webhook/admission-registry/tls/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	cryptorand "crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -9,6 +10,11 @@ import (
 	"encoding/pem"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"math/big"
 	"os"
 	"time"
@@ -133,6 +139,146 @@ func main() {
 		log.Panic(err)
 	}
 
+	log.Println("webhook server的tls服务完成")
+
+	// caBundle设置需要caPEM
+	if err := CreateAdmissionConfig(caPEM); err != nil {
+		log.Panic(err)
+	}
+	log.Println("webhook的configuration资源对象生成成功")
+
+}
+
+// 自动注册准入控制器,即validate mutate两种资源
+func CreateAdmissionConfig(caCert *bytes.Buffer) error {
+	// 针对前面的webhook处理,配置已知,也可以从环境变量中获取
+	webhookNamespace, _ := os.LookupEnv("WEBHOOK_NAMESPACE")
+	validateConfigName, _ := os.LookupEnv("VALIDATE_CONFIG")
+	mutateConfigName, _ := os.LookupEnv("MUTATE_CONFIG")
+	webhookService, _ := os.LookupEnv("WEBHOOK_SERVICE")
+	validatePath, _ := os.LookupEnv("VALIDATE_PATH")
+	mutatePath, _ := os.LookupEnv("MUTATE_PATH")
+
+	ctx := context.Background()
+	clientset, _ := InitKubernetesCli()
+	// 不为空则创建
+	// admissionregistration
+	if validateConfigName != "" {
+		validateConfig := &admissionregistrationv1.ValidatingWebhookConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: validateConfigName,
+			},
+			Webhooks: []admissionregistrationv1.ValidatingWebhook{
+				{
+					Name: "io.ydzs.admission-registry",
+					ClientConfig: admissionregistrationv1.WebhookClientConfig{
+						// []byte yaml就是ca.pem的base64编码
+						CABundle: caCert.Bytes(),
+						Service: &admissionregistrationv1.ServiceReference{
+							Name:      webhookService,
+							Namespace: webhookNamespace,
+							Path:      &validatePath,
+						},
+					},
+					Rules: []admissionregistrationv1.RuleWithOperations{
+						{
+							// create update delete
+							Operations: []admissionregistrationv1.OperationType{admissionregistrationv1.Create},
+							Rule: admissionregistrationv1.Rule{
+								APIGroups:   []string{""},
+								APIVersions: []string{"v1"},
+								Resources:   []string{"pods"},
+							},
+						},
+					},
+					FailurePolicy: func() *admissionregistrationv1.FailurePolicyType {
+						pt := admissionregistrationv1.Fail
+						return &pt
+					}(),
+					AdmissionReviewVersions: []string{"v1"},
+					// none
+					SideEffects: func() *admissionregistrationv1.SideEffectClass {
+						se := admissionregistrationv1.SideEffectClassNone
+						return &se
+					}(),
+				},
+			},
+		}
+		validateAdmissionClient := clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations()
+		_, err := validateAdmissionClient.Get(ctx, validateConfigName, metav1.GetOptions{})
+		if err != nil {
+			// ValidatingWebhookConfiguration错误是不存在
+			if errors.IsNotFound(err) {
+				if _, err = validateAdmissionClient.Create(ctx, validateConfig, metav1.CreateOptions{}); err != nil {
+					return err
+				}
+			} else {
+				return err
+			}
+		} else { // 存在,更新
+			if _, err = validateAdmissionClient.Update(ctx, validateConfig, metav1.UpdateOptions{}); err != nil {
+				return err
+			}
+		}
+	}
+
+	if mutateConfigName != "" {
+		mutateConfig := &admissionregistrationv1.MutatingWebhookConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: mutateConfigName,
+			},
+			Webhooks: []admissionregistrationv1.MutatingWebhook{
+				{
+					Name: "io.ydzs.admission-registry-mutate",
+					ClientConfig: admissionregistrationv1.WebhookClientConfig{
+						CABundle: caCert.Bytes(), // CA bundle created earlier
+						Service: &admissionregistrationv1.ServiceReference{
+							Name:      webhookService,
+							Namespace: webhookNamespace,
+							Path:      &mutatePath,
+						},
+					},
+					Rules: []admissionregistrationv1.RuleWithOperations{
+						{
+							Operations: []admissionregistrationv1.OperationType{
+								admissionregistrationv1.Create},
+							Rule: admissionregistrationv1.Rule{
+								APIGroups:   []string{"apps", ""},
+								APIVersions: []string{"v1"},
+								Resources:   []string{"deployments", "services"},
+							},
+						},
+					},
+					FailurePolicy: func() *admissionregistrationv1.FailurePolicyType {
+						pt := admissionregistrationv1.Fail
+						return &pt
+					}(),
+					AdmissionReviewVersions: []string{"v1"},
+					SideEffects: func() *admissionregistrationv1.SideEffectClass {
+						se := admissionregistrationv1.SideEffectClassNone
+						return &se
+					}(),
+				},
+			},
+		}
+	}
+
+	mutateAdmissionClient := clientset.AdmissionregistrationV1().MutatingWebhookConfigurations()
+	_, err := mutateAdmissionClient.Get(ctx, mutationCfgName, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			if _, err = mutateAdmissionClient.Create(ctx, mutateConfig, metav1.CreateOptions{}); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	} else {
+		if _, err = mutateAdmissionClient.Update(ctx, mutateConfig, metav1.UpdateOptions{}); err != nil {
+			return err
+		}
+	}
+}
 }
 
 // WriteFile writes data in the file at the given path
@@ -148,4 +294,20 @@ func WriteFile(filepath string, sCert *bytes.Buffer) error {
 		return err
 	}
 	return nil
+}
+
+// 初始化kubernetes的客户端工具,操作集群资源
+func InitKubernetesCli() (*kubernetes.Clientset, error) {
+	var (
+		config *rest.Config
+		err    error
+	)
+	if config, err = rest.InClusterConfig(); err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
 }
